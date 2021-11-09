8588190abd0ff347f6745729a6935285bdb6f91e
Sivanesh S <86925605+narc-kany@users.noreply.github.com>
2021-11-09 13:34:05 +0530

    Add files via upload

---
+++ auto-save-function/auto-save-function/README.markdown
@@ -0,0 +1,5 @@
+# Auto-Save function
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/narckany/pen/dyzKyLo](https://codepen.io/narckany/pen/dyzKyLo).
+
+
---
+++ auto-save-function/auto-save-function/dist/script.js
@@ -0,0 +1,27 @@
+function validateFloatKeyPress(el, evt) {
+    var charCode = (evt.which) ? evt.which : event.keyCode;
+    var number = el.value.split('.');
+    if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    //just one dot
+    if(number.length>1 && charCode == 46){
+         return false;
+    }
+    //get the carat position
+    var caratPos = getSelectionStart(el);
+    var dotPos = el.value.indexOf(".");
+    if( caratPos > dotPos && dotPos>-1 && (number[1].length > 1)){
+        return false;
+    }
+    return true;
+}
+
+function getSelectionStart(o) {
+	if (o.createTextRange) {
+		var r = document.selection.createRange().duplicate()
+		r.moveEnd('character', o.value.length)
+		if (r.text == '') return o.value.length
+		return o.value.lastIndexOf(r.text)
+	} else return o.selectionStart
+}
